ce2ccb32dd3fbc462eb9af34cc6d00194d945dbf
colljos <jose.coll@lendable.co.uk>
2021-12-03 18:50:17 +0000

    Initial commit creating project from Spring initializr

---
+++ src/test/kotlin/com/project/spring/reactive/template/TemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.spring.reactive.template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
